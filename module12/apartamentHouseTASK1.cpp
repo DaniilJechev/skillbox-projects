9855be62535b3b252bbf5b1a2b759763dca6dc91
Daniil Jechev <daniilzhechev.bfg@gmail.com>
2023-08-26 18:40:26 +0300

    Task number 1 completed

--- module12/apartamentHouseTASK1.cpp
+++ module12/apartamentHouseTASK1.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
-    std::cout << "Hello world! hi";
+    int number;
+    std::string houseResidents[] = {"SidorovA" , "IvanovA", "PetrovA", "SidorovB",
+     "IvanovB", "PetrovB", "SidorovC" , "IvanovC", "PetrovC", "SidorovD"};
+
+    std::cout << "Enter the apartament numbers 3 times: \n";
+    for (int i = 0; i < 3; i++){
+        std::cin >> number;
+        while ( number < 1 || number > 10){
+            std::cout << "Incorrect\nEnter new number: ";
+            std::cin >> number;
+        }
+        std::cout << houseResidents[number + 1] << " is living in apartament number " << number << std::endl;
+    }
 }
