79df6c0fcb5422683cec2fee1844a37b12e87eca
Daniil Jechev <daniilzhechev.bfg@gmail.com>
2023-09-04 22:22:20 +0300

    Ver 2 (add the ships builder and that almost running

--- module14/seaBattle_Task8.cpp
+++ module14/seaBattle_Task8.cpp
@@ -2,17 +2,25 @@
 #include <vector>
 #include <iomanip>
 
-void battlePrint (std::vector<std::vector<int>> const &field, std::string const &player)   //field display during battle
+void battlePrint (std::vector<std::vector<int>> const &field, std::string const )   //field display during battle
 {
-    std::cout << player << "\n          |\n          ↓\n";
     for (int i = 0; i < field.size(); i ++){
         for (int j = 0; j < field[0].size(); j ++){
-            if (field[i][j] != 3) {
+            if (i == 0 && j == 0){
+                std::cout << std::setw(4) << ' ';
+            } else if (i == 0 || j == 0) {
+                std::cout << std::setw(3) << field[i][j];
+            }else if (field[i][j] == 0) {
                 std::cout << std::setw(3) << '.';
-            }      
+            }else std::cout << std::setw(3) << field[i][j];
+            if (j == field[0].size() - 1 && i == 0){
+                std::cout << "\n      ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━";
+            }else if (j == 0 && i > 0) std::cout <<" ┃";
+                   
         }
-        std::cout << std::endl;
+        std::cout << "\n";
     }
+    std::cout << "\n";
 }
 
 void placeMentPrint (std::vector<std::vector<int>> const &field)                           //display of the field during the construction of ship
@@ -20,35 +28,72 @@ void placeMentPrint (std::vector<std::vector<int>> const &field)                
     for (int i = 0; i < field.size(); i ++){
         for (int j = 0; j < field[0].size(); j ++){
             if (i == 0 && j == 0){
-                std::cout << std::setw(3) << ' ';
+                std::cout << std::setw(5) << ' ';
             } else if (i == 0 || j == 0) {
                 std::cout << std::setw(3) << field[i][j];
             }else if (field[i][j] == 0) {
                 std::cout << std::setw(3) << '.';
-            }else std::cout << std::setw(3) << field[i][j];      
+            }else std::cout << std::setw(3) << field[i][j];
+            if (j == field[0].size() - 1 && i == 0){
+                std::cout << "\n      ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━";
+            }else if (j == 0 && i > 0) std::cout <<" ┃";
+                   
         }
-        std::cout << std::endl;
+        std::cout << "\n";
     }
+    std::cout << "\n";
 }
 
-bool isAllowed(std::vector<std::vector<int>> &field,int &size, int &x1, int &y1, int &x2, int &y2 )    //will check game rules
+bool circleCheck(std::vector<std::vector<int>> &field, int &x, int &y)
 {
-    if (( y1 > 0 && y1 <= 10 && x1 > 0 && x1 <= 10)
-          && (y2 > 0 && y2 <= 10 && x2 > 0 && x2 <= 10)) return false;
-
-    if (((y1 == y2) && ( abs(x1 - x2) != size))
-          || (x1 == x2) && ( abs(y1 - y2) != size))return false;
-
-    for (int i = 0; i < size; i ++){
-        if (1);
+    for (int i = x - 1; i < x + 1; i ++){
+        for (int j = y - 1; j < y + 1; j ++) 
+            if (field[i][j]) return false;
     }
     return true;
 }
 
-void shipBuilding (std::vector<std::vector<int>> &field,int &size, int &count)                            //That function will build a ship
+bool isAllowed(std::vector<std::vector<int>> &field,int &size, int &x1, int &y1, int &x2, int &y2 )    //will check game rules
 {
-    int x1, y1, x2, y2;
+    if (!(( y1 > 0 && y1 <= 10 && x1 > 0 && x1 <= 10)
+          && (y2 > 0 && y2 <= 10 && x2 > 0 && x2 <= 10))) return false;
+
+    if (((y1 == y2) && ( abs(x1 - x2) == size))
+          || ((x1 == x2) && ( abs(y1 - y2) == size))
+          || ((y1 != y2) && (x1 != x2))) return false;
+    
+    if (y2 < y1) std::swap(y1,y2);
+    if (x2 < x1) std::swap(x1,x2);
+
+    if (y1 == y2){
+        for (int i = x1; i < x2 + 1; i ++){
+            if (circleCheck(field,i,y1 )) return false;
+        }
+    }else if ( x1 == x2 ){ 
+        for (int i = y1; i < y2 + 1; i ++){
+            if (circleCheck(field, x2, i)) return false;
+        }
+    }else return false;
+    return true;
+}
+
+void build (std::vector<std::vector<int>> &field,int &size, int &x1, int &y1, int &x2, int &y2)       //Build a ship
+{
+    if (y1 == y2){
+        for (int i = x1; i < x1 + size; i ++){
+            field[y1][i] = size;
+        }
+    }else for (int i = y1; i < y2 + size - 1; i ++){
+            field[i][x1] = size;
+        }
+}
+
+void shipBuilding (std::vector<std::vector<int>> &field,int &size, int &count)               //That function will build a ship
+{
+    int x1, y1 = 0, x2, y2;
+    if ( size < 6 ){
     std::cout <<"You need to arrange " << count << " ship with the size of " << size << " cell.\n(input coordinates)\n";
+    placeMentPrint(field);
 
     for (;count > 0; count--){
         std::cout << "Input first coordinates ";
@@ -56,23 +101,26 @@ void shipBuilding (std::vector<std::vector<int>> &field,int &size, int &count)  
         std::cout << "Input second coordinates ";
         std::cin >> x2 >> y2;
         if (isAllowed(field, size, x1, y1, x2, y2)){
-            field[y1][x1] = size;
+            build(field, size ,x1, y1, x2, y2);
         }else {
-            std::cout << "\nError, try again\n";
+            std::cout << "Error, try again\n";
             shipBuilding(field, size, count);
         }
         placeMentPrint(field);
+        if (count <= 0 || size == 6) return;
     }
 
     size += 1;
     count = 5 - size;
     shipBuilding(field, size, count);
+    }else return;
 }
 
 void shipBuilding_start (std::vector<std::vector<int>> &field, int &count)                 // This function is designed to run (when ship size = 1)
 {
     int x, y, size = 1;
     std::cout <<"You need to arrange " << count << " ship with the size of " << size << " cell.\n(input coordinates)\n";
+    placeMentPrint(field);
 
     for (;count > 0; count--){
         std::cout << "Input coordinates ";
@@ -87,7 +135,7 @@ void shipBuilding_start (std::vector<std::vector<int>> &field, int &count)      
         field[y][x] = size;
         placeMentPrint(field);
     }
-    size += 1;
+   size += 1;
     count = 5 - size;
     shipBuilding(field, size, count);
 }
@@ -100,11 +148,11 @@ void designations (std::vector<std::vector<int>> &field)              //designat
     }
 }
 
-void placeMent (std::vector<std::vector<int>> &field, std::string const &player)         //This function places ships (using helper functions)
+void placeMent (std::vector<std::vector<int>> &field, std::string const player )         //This function places ships (using helper functions)
 {
     int count = 4;    //This variable shows the number of ships of a certain size not yet built.
-    std::cout << "You are a "<< player <<" player.\n";
-    std::cout << "   Thats your field" << "\n          |\n          ↓\n";
+    std::cout << "        You are a "<< player <<" player.\n";
+    std::cout << "           Thats your field" << "\n                  |\n                  ↓\n";
     placeMentPrint(field);
     shipBuilding_start(field, count);
 }
@@ -113,12 +161,21 @@ int main ()
 {
     std::vector <std::vector<int>> field_1 (11, std::vector <int>(11, 0));
     std::vector <std::vector<int>> field_2 (11, std::vector <int>(11, 0));
+    std::string p1, p2;
+    std::cout << "Enter first player name ";
+    std::cin >> p1;
+    std::cout << "Enter second player name ";
+    std::cin >> p2;
 
     designations(field_1);
     designations(field_2);
     
     std::cout << "Hello, this is a sea battle.\nFirst, you need to arrange your ships for two players.\n\n";
 
-    placeMent(field_1, "first");
-    placeMent(field_2, "second");
+    placeMent(field_1, p1);
+    placeMent(field_2, p2);
+
+    for (;;){
+        std::cout <<"\n\n\n\n\n\n\nLets start!";
+    }
 }
