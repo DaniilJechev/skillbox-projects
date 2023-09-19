fd510ee0bed3a6181d6def6625eac80f3adb8ac5
Daniil Jechev <daniilzhechev.bfg@gmail.com>
2023-09-19 18:30:25 +0300

    speedometr result

--- speedometr_Task1.cpp
+++ speedometr_Task1.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int main ()
-{
-    std::cout << "Hello world!";
+int main (){
+    float deltaF;
+    char del[4], speed[4];
+    do{
+        std::cout << "\nEnter yout delta: ";
+        std::cin >> deltaF;
+        std::sprintf(del, "%.2f", deltaF);
+        std::sprintf(speed, "%.1f", atof(speed) + atof(del));
+        std::cout << std::endl << "Your speed = " << speed;
+    } while (atof(speed) >= 0 && atof(speed) <= 150);
+   
+    if (atoi(speed) > 150) std::cout << std::endl << "You crashed";
+    else std::cout << std::endl << "You stalled";
 }
