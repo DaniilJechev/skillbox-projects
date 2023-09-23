5ef14e2a3811bd2f95175a3ac00510d43cad2616
Daniil Jechev <daniilzhechev.bfg@gmail.com>
2023-09-23 20:23:01 +0300

    piano change

--- MechanicalPiano_Task4.cpp
+++ MechanicalPiano_Task4.cpp
@@ -5,27 +5,27 @@
 enum note
 {
     DO = 1, 
-    RE = 2, 
-    MI = 4, 
-    FA = 8, 
-    SOL = 16, 
-    LA = 32, 
-    SI = 64
+    RE, 
+    MI, 
+    FA, 
+    SOL, 
+    LA, 
+    SI
 };
 
 bool isAllowed (int combination)
 {
-    return !(combination <= 127 && combination >= 0);
+    return !(combination <= 7 && combination >= 1);
 }
 
 void melodyPrint (int sound){
-        if (sound & note::DO) std::cout << "Do ", Sleep(200);
-        if (sound & note::RE) std::cout << "RE ", Sleep(200);
-        if (sound & note::MI) std::cout << "MI ", Sleep(200);
-        if (sound & note::FA) std::cout << "FA ", Sleep(200);
-        if (sound & note::SOL) std::cout << "SOL ", Sleep(200);
-        if (sound & note::LA) std::cout << "LA ", Sleep(200);
-        if (sound & note::SI) std::cout << "SI", Sleep(200);
+        if (sound == note::DO) std::cout << "Do ", Sleep(200);
+        if (sound == note::RE) std::cout << "RE ", Sleep(200);
+        if (sound == note::MI) std::cout << "MI ", Sleep(200);
+        if (sound == note::FA) std::cout << "FA ", Sleep(200);
+        if (sound == note::SOL) std::cout << "SOL ", Sleep(200);
+        if (sound == note::LA) std::cout << "LA ", Sleep(200);
+        if (sound == note::SI) std::cout << "SI", Sleep(200);
     std::cout << std::endl;
 }
 
